3538c9d086c1c5abd41c97be962911fd86cdc8f9
Ryan Lundy <ryan.lundy@unblu.com>
2020-12-02 07:18:10 +0100

    Finish year 2020 day 2 part 1 in F#.

--- net/AdventOfCodeFs/Tests2020.fs
+++ net/AdventOfCodeFs/Tests2020.fs
@@ -22,3 +22,22 @@ let ``Year 2020 day 1 part 2`` () =
 let ``Year 2020 day 1 solutions`` () =
     Debug.Print(Year2020.Day1.input |> Year2020.Day1.part1 |> fun x -> x.ToString())
     Debug.Print(Year2020.Day1.input |> Year2020.Day1.part2 |> fun x -> x.ToString())
+
+[<Fact>]
+let ``Year 2020 day 2 part 1`` () =
+    let input = "1-3 a: abcde,1-3 b: cdefg,2-9 c: ccccccccc"
+    let expected = 2L
+    let actual = Year2020.Day2.part1 input
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Year 2020 day 2 part 2`` () =
+    let input = "1-3 a: abcde,1-3 b: cdefg,2-9 c: ccccccccc"
+    let expected = 1L
+    let actual = Year2020.Day2.part2 input
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Year 2020 day 2 solutions`` () =
+    Debug.Print(Year2020.Day2.input |> Year2020.Day2.part1 |> fun x -> x.ToString())
+    Debug.Print(Year2020.Day2.input |> Year2020.Day2.part2 |> fun x -> x.ToString())
