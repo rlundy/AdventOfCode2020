51fbdc6563bec46864d6b8aef0c529b3fd277312
Ryan Lundy <ryan.lundy@unblu.com>
2020-12-02 07:16:48 +0100

    Un-hard-code types in ApplyTests.

--- net/AdventOfCodeCs/Helpers.cs
+++ net/AdventOfCodeCs/Helpers.cs
@@ -6,7 +6,7 @@ namespace AdventOfCodeCs
 {
     public static class Helpers
     {
-        public static void ApplyTests(this IEnumerable<(string, long)> inputs, Func<string, long> function)
+        public static void ApplyTests<TIn, TOut>(this IEnumerable<(TIn, TOut)> inputs, Func<TIn, TOut> function)
         {
             foreach (var (input, expected) in inputs)
                 Assert.Equal(expected, function(input));
