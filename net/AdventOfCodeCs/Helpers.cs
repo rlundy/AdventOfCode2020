57c4bb1bbd87532382db55ce82568b62191ec081
Ryan Lundy <ryan.lundy@unblu.com>
2020-12-06 10:18:57 +0100

    Add method to get puzzle input from files; add method to show banner per day.

--- net/AdventOfCodeCs/Helpers.cs
+++ net/AdventOfCodeCs/Helpers.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using Xunit;
 
 namespace AdventOfCodeCs
@@ -10,6 +12,22 @@ namespace AdventOfCodeCs
         {
             foreach (var (input, expected) in inputs)
                 Assert.Equal(expected, function(input));
+        }
+
+        public static void Banner(long year, long day) => Debug.Print($"*** Year {year} Day {day} ***");
+    }
+
+    public static class PuzzleInput
+    {
+        public static string Get(long year, long day)
+        {
+            var currentDir = new DirectoryInfo(".");
+            Debug.Print($"Current folder: {currentDir.FullName}");
+            var folder = new DirectoryInfo(@"..\..\..\..\..\PuzzleInput");
+            var expectedFileName = $"y{year}d{day.ToString("00")}.txt";
+            var fileName = Path.Combine(folder.FullName, expectedFileName);
+            var file = new FileInfo(fileName);
+            return File.ReadAllText(file.FullName);
         }
     }
 }
